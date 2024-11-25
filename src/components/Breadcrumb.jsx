fea1792f9635252470013d7241089ded826be0c8
indiedanish <dbdanishbutt@gmail.com>
2024-11-26 00:27:41 +0500

    fix

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -6,7 +6,7 @@ const Breadcrumb = ({ title }) => {
     <div className="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
       <div className="d-flex flex-column ">
 
-      <h6 className="fw-semibold mb-0">  UK Sponsored work visas by occupation and industry [Q1 2022 - 2024 Q3]</h6>
+      <h6 className="fw-semibold mb-0">  UK Sponsored work visas by occupation and industry [Q1 2022 - Q3 2024]</h6>
       <span className=" mb-0">All datasets have been scrapped directly from the official government websites. **Limited to publicly available data**</span>
       </div>
 
